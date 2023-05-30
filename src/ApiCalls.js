7728282beb7f28c37a4a4414d2d5cc31abaadbb7
“Sarah <sarahcatherine311@gmail.com>
2023-05-30 09:27:34 -0600

    feat: Create file structure and complete GET fetch request

---
+++ src/ApiCalls.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const fetchData = () => {
+  return fetch("http://localhost:3001/api/v1/reservations")
+    .then(response => response.json())    
+}
+
+export default fetchData;
--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import fetchData from '../ApiCalls';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+  
+  componentDidMount() {
+
+    fetchData()
+    .then(data => this.setState({reservations: data}))
+    
+  }
+  
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form />
         </div>
         <div className='resy-container'>
-          
+          <Reservations />
         </div>
       </div>
     )
